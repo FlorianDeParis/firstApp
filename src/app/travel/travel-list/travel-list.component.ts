6a50e172ea12dd4188dd206578d51bcdd8b790c8
Florian <florian.tournay@hotmail.fr>
2025-07-22 11:58:41 +0200

    Pipe async subscription

--- src/app/travel/travel-list/travel-list.component.ts
+++ src/app/travel/travel-list/travel-list.component.ts
@@ -20,9 +20,26 @@ export class TravelListComponent implements OnInit{
 
   // Methode cycle de vie du component
   // Exécutée automatiquement par Angular une seule fois
+  // Avec Async/Await
   async ngOnInit() {
-    this.travels = await this.dataService.getTravelsAsync("");
+    // Gestion de la promesse explicite avec détection des changements
+    // this.travels = await this.dataService.getTravelsAsync("");
+
+    // Pas d'attente -> je compte sur le pipe async pour attendre et mettre à jour l'UI
+    // qui dépend du résultat de la promesse
+    // pas besoin de la détection des changements => le pipe s'en charge
+    this.$travels = this.dataService.getTravelsAsync("");
+  }
+
+  //Avec Callback
+
+  ngOnInit2() {
+
+    this.dataService.getTravelsAsync("").then( r => {
+      this.travels = r;
+    });
   }
 
   travels?: Travel[];
+  $travels?: Promise<Travel[]>
 }
