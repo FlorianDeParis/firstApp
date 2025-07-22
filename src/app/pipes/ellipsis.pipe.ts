42e9d3025d5f56c6df864a219be7e12ddf09ba87
Florian <florian.tournay@hotmail.fr>
2025-07-22 09:46:39 +0200

    fixup! custom pipe with InjectionToken / with value stored in provider

--- src/app/pipes/ellipsis.pipe.ts
+++ src/app/pipes/ellipsis.pipe.ts
@@ -1,4 +1,4 @@
-import { InjectionToken, Pipe, PipeTransform } from '@angular/core';
+import { inject, Inject, InjectionToken, Pipe, PipeTransform } from '@angular/core';
 
 // Pour definir l'information nbCharMax de façon souple (faiblement couplée) et globale
 // utiliser l'injection de dépendance
@@ -13,10 +13,27 @@ export const EllipsisPipeMaxCharacters =new InjectionToken("EllipsisPipeMaxChara
 })
 export class EllipsisPipe implements PipeTransform {
 
-  transform(value: string, nbCharMax:number=10): string {
+  // Obtention d'une dépendance dans le constructeur
+  // constructor(@Inject(EllipsisPipeMaxCharacters) private defaultNbCharMaxValue:number){
+
+  // }
+
+  // Autre façon d'obtenir une dépendance
+  defaultNbCharMaxValue = inject(EllipsisPipeMaxCharacters) as number;
+
+  transform(value: string, nbCharMax?:number): string {
+
+    // Soit nbCharMax est fourni dans le template => sa valeur est utilisée
+    // sinon, on prend la valeur deonnée par l'injection de dépendance
+    if(!nbCharMax){
+      nbCharMax = this.defaultNbCharMaxValue;
+    }
     if(value.length <= nbCharMax){
       return value;
     }
+
+    // Je tronque la chaine et ajoute ...
+    return value.substring(0, nbCharMax-3)+"...";
   }
 
 }
